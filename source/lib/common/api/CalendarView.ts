f4522b6bcc0f6b29586af7976503360e1cdf9a97
Patrick <patrykrzucidlo1@gmail.com>
2020-09-02 22:39:40 +0200

    Add getDateRange field to CalendarView interface

--- source/lib/common/api/CalendarView.ts
+++ source/lib/common/api/CalendarView.ts
@@ -4,19 +4,26 @@ import { DateChangeAction } from './DateChangeAction';
 
 export interface CalendarView {
     /**
-     * Name of the view.
+     *  Name of the view in different locales, if such are supported.
      */
     name: {
         [localName: string]: string;
     };
 
     /**
-     * Component that represents the view.
+     *  Component that represents the view.
      */
     component: (calendarViewProps: CalendarViewProps) => ReactElement<CalendarViewProps>;
 
     /**
-     * Change the date highlighted by the calendar depending on user input.
+     *  Date range required by the view.
+     *
+     *  eg. MonthView requires "from" to be start of the month and "till" to be end of the month
+     */
+    getDateRange: (highlightDate: Date) => { from: Date; till: Date };
+
+    /**
+     *  Change the date highlighted by the calendar depending on user input.
      */
     onDateChange?: (
         dateChangeAction: DateChangeAction.BACKWARD | DateChangeAction.FORWARD,
