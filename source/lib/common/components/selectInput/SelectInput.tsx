1b3430a5ea094bf827f35e7e8fbebe8ad79fcb67
Patrick <patrykrzucidlo1@gmail.com>
2020-09-02 20:36:00 +0200

    Clean up SelectInput component

--- source/lib/common/components/selectInput/SelectInput.tsx
+++ source/lib/common/components/selectInput/SelectInput.tsx
@@ -1,83 +1,70 @@
 import { FormControl, MenuItem, Select } from '@material-ui/core';
-import React, { CSSProperties, ReactElement, useEffect, useState } from 'react';
+import { find } from 'lodash';
+import React, { ReactElement, useEffect, useState } from 'react';
 
-export type SelectInputValueType = string | number | string[];
+export type SelectInputValue = string | number | string[];
 
-export interface SelectInputOptions {
-    [name: string]: SelectInputValueType;
+export interface SelectOption<T> {
+    id?: number;
+
+    name: SelectInputValue;
+
+    source: T;
 }
 
-interface Props {
-    label?: string;
-    options: SelectInputOptions;
-    fullWidth?: boolean;
-    defaultFirstItem?: boolean;
-    style?: CSSProperties;
+interface SelectInputProps<T> {
+    options: SelectOption<T>[];
     variant?: 'filled' | 'outlined' | 'standard';
-    labelWidth?: number;
-    autoWidth?: boolean;
-    displayNone?: boolean;
 
-    onInputChange?: (value: SelectInputValueType) => void;
+    onInputChange?: (value: T) => void;
 }
 
-function SelectInput(props: Props) {
-    const [option, setOption] = useState('' as SelectInputValueType);
+function SelectInput<T>(props: SelectInputProps<T>) {
+    const [options, setOptions] = useState<SelectOption<T>[]>([]);
+    const [optionId, setOptionId] = useState(0);
 
     /**
      * If setDefaultAsFirstItem flag is set, then this method does exactly that.
      */
     useEffect(() => {
-        if (props.defaultFirstItem) {
-            const optionsKeys = Object.keys(props.options);
+        assignItemsId();
+    }, [props.options]);
 
-            if (optionsKeys.length > 0) {
-                setOption(props.options[optionsKeys[0]]);
-            }
-        }
-    }, []);
+    function assignItemsId() {
+        setOptions(
+            props.options.map((inputItem, index) => {
+                inputItem.id = index;
 
-    /**
-     * Calls onChange prop each time option value changes.
-     */
-    useEffect(() => {
-        if (option !== '') {
-            if (props.onInputChange) {
-                for (let key of Object.keys(props.options)) {
-                    if (props.options[key] === option) {
-                        props.onInputChange(key);
-                    }
-                }
-            }
-        }
-    }, [option]);
+                return inputItem;
+            }),
+        );
+    }
 
     function handleChange(event: React.ChangeEvent<{ value: unknown }>) {
-        setOption(event.target.value as SelectInputValueType);
+        const id: number = event.target.value as number;
+
+        setOptionId(id);
+
+        if (props.onInputChange) {
+            const selectInputValue = find(options, { id: id });
+
+            if (selectInputValue) {
+                props.onInputChange(selectInputValue.source);
+            }
+        }
     }
 
     function renderMenuOptions(): ReactElement[] {
-        let menuOptions: ReactElement[] = [];
-
-        for (let selectInputOption of Object.keys(props.options)) {
-            menuOptions.push(
-                <MenuItem key={selectInputOption} value={props.options[selectInputOption]}>
-                    {props.options[selectInputOption]}
-                </MenuItem>,
-            );
-        }
-
-        return menuOptions;
+        return options.map((inputItem) => (
+            <MenuItem key={inputItem.id} value={inputItem.id}>
+                {inputItem.name}
+            </MenuItem>
+        ));
     }
 
     return (
         <FormControl>
-            <Select value={option} margin="dense" onChange={handleChange} variant="outlined">
-                {props.displayNone ? (
-                    <MenuItem value="">
-                        <em>None</em>
-                    </MenuItem>
-                ) : null}
+            <Select value={optionId} margin="dense" onChange={handleChange} variant={props.variant}>
                 {renderMenuOptions()}
             </Select>
         </FormControl>
