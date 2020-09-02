c47c03746eb78b22ca0df9233dfdf3b7edbb8bcd
Patrick <patrykrzucidlo1@gmail.com>
2020-09-02 19:39:15 +0200

    Add useCalendarContext hook

---
+++ source/lib/common/hooks/context/useCalendarContext.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import { CalendarView } from '../../api/CalendarView';
+import { EventStorage } from '../../api/EventStorage';
+import { CalendarContextStructure } from '../../contexts/CalendarContext';
+
+export default function useCalendarContext(): CalendarContextStructure {
+    const [eventStorage, setEventStorage] = useState<EventStorage | null>(null);
+    const [views, setViews] = useState<CalendarView[]>([]);
+
+    return {
+        eventStorage,
+        views,
+
+        setEventStorage,
+        setViews,
+    };
+}
