27691b74b06b96cbca089856faa9926ed3f688aa
Patrick <patrykrzucidlo1@gmail.com>
2020-09-03 19:23:50 +0200

    Add basic version of useCalendarEventStorage

---
+++ source/lib/core/components/eventStorage/useCalendarEventStorage.tsx
@@ -0,0 +1,73 @@
+import { useEffect } from 'react';
+import CalendarEvent from '../../../common/api/CalendarEvent';
+import { EventStorage } from '../../../common/api/EventStorage';
+import { CalendarContextStructure } from '../../../common/contexts/CalendarContext';
+import ViewContextStructure from '../../../common/contexts/ViewContext';
+import EventStoragePresenceHelper from './EventStoragePresenceHelper';
+
+export interface UseCalendarEventStorageProps {
+    onDataRequest: (from: Date, till: Date) => Promise<CalendarEvent[]>;
+
+    calendarContext: CalendarContextStructure;
+    viewContext: ViewContextStructure;
+}
+
+export default function useCalendarEventStorage(props: UseCalendarEventStorageProps) {
+    const calendarContext: CalendarContextStructure = props.calendarContext;
+    const viewContext: ViewContextStructure = props.viewContext;
+
+    useEffect(() => {
+        loadData();
+    }, [viewContext.highlightDate, viewContext.view]);
+
+    async function loadData(): Promise<void> {
+        if (viewContext.view) {
+            const dateRange = viewContext.view.getDateRange(viewContext.highlightDate);
+
+            if (!isDataPresentInStorage(dateRange.from, dateRange.till)) {
+                const requestedData: CalendarEvent[] = await props.onDataRequest(dateRange.from, dateRange.till);
+
+                calendarContext.setEventStorage(sortEvents(requestedData));
+            } else {
+                console.log('nothing new');
+            }
+        }
+    }
+
+    function isDataPresentInStorage(from: Date, till: Date): boolean {
+        return EventStoragePresenceHelper.isDataPresent(from, till, calendarContext.eventStorage);
+    }
+
+    function sortEvents(calendarEvents: CalendarEvent[]): EventStorage {
+        let eventStorage: EventStorage = calendarContext.eventStorage;
+
+        for (let event of calendarEvents) {
+            const year = event.startedAt.getFullYear();
+            const month = event.startedAt.getMonth();
+            const day = event.startedAt.getDate();
+
+            eventStorage[year] = {
+                ...eventStorage[year],
+                [month]: {
+                    ...eventStorage[year]?.[month],
+                    [day]: [
+                        ...(eventStorage[year]?.[month]?.[day] === undefined ? [] : eventStorage[year]?.[month]?.[day]),
+                        event,
+                    ],
+                },
+            };
+        }
+
+        return eventStorage;
+    }
+
+    return new (class {
+        /**
+         * Forces EventStorage to request data once again.
+         */
+        public async forceReload(calendarEvent?: []): Promise<void> {
+            if (calendarEvent) {
+            }
+        }
+    })();
+}
