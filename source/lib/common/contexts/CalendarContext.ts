62e24e9139978d033ce0432278a1de9d7ea1de6c
Patrick <patrykrzucidlo1@gmail.com>
2020-09-06 14:40:44 +0200

    Add LocaleSource to CalendarContext

--- source/lib/common/contexts/CalendarContext.ts
+++ source/lib/common/contexts/CalendarContext.ts
@@ -1,24 +1,31 @@
 import React from 'react';
 import localeList from '../../locale/LocaleList';
+import localization, { LocaleSource } from '../../locale/Localization';
 import { CalendarView } from '../api/CalendarView';
 import { EventStorage } from '../api/EventStorage';
 
 export interface CalendarContextStructure {
     locale: string;
+    localeSource: LocaleSource;
+
     eventStorage: EventStorage;
     views: CalendarView[];
 
     setLocale: (locale: string) => void;
+    setLocaleSource: (localeSource: LocaleSource) => void;
     setEventStorage: (eventStorage: EventStorage) => void;
     setViews: (views: CalendarView[]) => void;
 }
 
 const DEFAULT_CONTEXT: CalendarContextStructure = {
     locale: localeList.enUS,
+    localeSource: localization,
+
     eventStorage: {},
     views: [],
 
     setLocale: () => {},
+    setLocaleSource: () => {},
     setEventStorage: () => {},
     setViews: () => {},
 };
