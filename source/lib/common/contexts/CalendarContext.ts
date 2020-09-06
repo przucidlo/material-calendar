b5967113f9e500dace6b9cb5d272038be8879efa
Patrick <patrykrzucidlo1@gmail.com>
2020-09-06 14:22:19 +0200

    Add locale field to CalendarContext

--- source/lib/common/contexts/CalendarContext.ts
+++ source/lib/common/contexts/CalendarContext.ts
@@ -1,19 +1,24 @@
 import React from 'react';
+import localeList from '../../language/LocaleList';
 import { CalendarView } from '../api/CalendarView';
 import { EventStorage } from '../api/EventStorage';
 
 export interface CalendarContextStructure {
+    locale: string;
     eventStorage: EventStorage;
     views: CalendarView[];
 
+    setLocale: (locale: string) => void;
     setEventStorage: (eventStorage: EventStorage) => void;
     setViews: (views: CalendarView[]) => void;
 }
 
 const DEFAULT_CONTEXT: CalendarContextStructure = {
+    locale: localeList.enUS,
     eventStorage: {},
     views: [],
 
+    setLocale: () => {},
     setEventStorage: () => {},
     setViews: () => {},
 };
