22b359de0c5ae169dedd57bd1042662feabf7c4c
Patrick <patrykrzucidlo1@gmail.com>
2020-09-06 19:23:38 +0200

    Split DayView code into DayContent component

--- source/lib/views/day/DayView.tsx
+++ source/lib/views/day/DayView.tsx
@@ -3,9 +3,8 @@ import CalendarEvent from '../../common/api/CalendarEvent';
 import CalendarViewProps from '../../common/api/CalendarViewProps';
 import { CalendarContext } from '../../common/contexts/CalendarContext';
 import { ViewContext } from '../../common/contexts/ViewContext';
-import TimeGrid from '../../core/components/timeGrid/TimeGrid';
+import DayContent from './components/dayContent/DayContent';
 import DayHeader from './components/dayHeader/DayHeader';
-import DayGrid from './DayGrid';
 
 export interface DayViewProps extends CalendarViewProps {}
 
@@ -19,7 +18,7 @@ export default function DayView(props: DayViewProps) {
      * If there is no events for that day function will
      * return empty array.
      */
-    function getDayEvents(): CalendarEvent[] {
+    function getEvents(): CalendarEvent[] {
         const dayDate: Date = viewContext.highlightDate;
         const dayEvents =
             calendarContext.eventStorage?.[dayDate.getFullYear()]?.[dayDate.getMonth()]?.[dayDate.getDate()];
@@ -30,19 +29,7 @@ export default function DayView(props: DayViewProps) {
     return (
         <div>
             <DayHeader highlightDate={viewContext.highlightDate} timeGridWidth={56} />
-            <div
-                style={{
-                    overflowY: 'scroll',
-                    height: `calc(100vh - 64px - 89px)`,
-                    display: 'flex',
-                    position: 'relative',
-                }}
-            >
-                <TimeGrid gridElementHeight={49} width={56} />
-                <div style={{ height: '100%', flexGrow: 1 }}>
-                    <DayGrid dayEvents={getDayEvents()} />
-                </div>
-            </div>
+            <DayContent events={getEvents()} />
         </div>
     );
 }
---
+++ source/lib/views/day/components/dayContent/DayContent.tsx
@@ -0,0 +1,35 @@
+import { makeStyles } from '@material-ui/core';
+import React, { ReactElement } from 'react';
+import CalendarEvent from '../../../../common/api/CalendarEvent';
+import TimeGrid from '../../../../core/components/timeGrid/TimeGrid';
+import DayGrid from '../../DayGrid';
+
+export interface DayContentProps {
+    events: CalendarEvent[];
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        overflowY: 'scroll',
+        height: `calc(100vh - 64px - 89px)`,
+        display: 'flex',
+        position: 'relative',
+    },
+    dayGrid: {
+        height: '100%',
+        flexGrow: 1,
+    },
+}));
+
+export default function DayContent(props: DayContentProps): ReactElement {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <TimeGrid gridElementHeight={49} width={56} />
+            <div className={classes.dayGrid}>
+                <DayGrid dayEvents={props.events} />
+            </div>
+        </div>
+    );
+}
