1089f5ddf34ccb593f2eff129a4a2fba8c231d65
Patrick <patrykrzucidlo1@gmail.com>
2020-09-06 20:57:35 +0200

    Remove deprecated locale files

--- source/lib/locale/DaysNames.tsx
+++
@@ -1,3 +0,0 @@
-export const daysNamesShort = ['Niedz.', 'Pon.', 'Wt.', 'Śr.', 'Czw.', 'Pt.', 'Sob.'];
-
-export const daysNames = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'];
