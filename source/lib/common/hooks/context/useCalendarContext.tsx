6e2ad3130ef80bea13a029cb72c31880bec0f8fd
Patrick <patrykrzucidlo1@gmail.com>
2020-09-06 14:52:17 +0200

    Update useCalendarContext hook to match new version of CalendarContext

--- source/lib/common/hooks/context/useCalendarContext.tsx
+++ source/lib/common/hooks/context/useCalendarContext.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
+import { LocaleSource } from '../../../locale/Localization';
 import { CalendarView } from '../../api/CalendarView';
 import { EventStorage } from '../../api/EventStorage';
-import { CalendarContextStructure } from '../../contexts/CalendarContext';
+import { CalendarContextStructure, DEFAULT_CALENDAR_CONTEXT } from '../../contexts/CalendarContext';
 
 export default function useCalendarContext(): CalendarContextStructure {
-    const [eventStorage, setEventStorage] = useState<EventStorage>({});
-    const [views, setViews] = useState<CalendarView[]>([]);
+    const [locale, setLocale] = useState<string>(DEFAULT_CALENDAR_CONTEXT.locale);
+    const [localeSource, setLocaleSource] = useState<LocaleSource>(DEFAULT_CALENDAR_CONTEXT.localeSource);
+    const [eventStorage, setEventStorage] = useState<EventStorage>(DEFAULT_CALENDAR_CONTEXT.eventStorage);
+    const [views, setViews] = useState<CalendarView[]>(DEFAULT_CALENDAR_CONTEXT.views);
 
     return {
+        locale,
+        localeSource,
         eventStorage,
         views,
 
+        setLocale,
+        setLocaleSource,
         setEventStorage,
         setViews,
     };
