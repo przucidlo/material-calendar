d254a3eb4da52d774403de28e9f0c785bd5dd582
Patrick <patrykrzucidlo1@gmail.com>
2020-09-07 10:09:45 +0200

    Add comments to EventStorageFiller

--- source/lib/core/components/eventStorage/EventStorageFiller.ts
+++ source/lib/core/components/eventStorage/EventStorageFiller.ts
@@ -2,9 +2,27 @@ import { eachDayOfInterval, endOfMonth, endOfWeek, startOfMonth, startOfWeek } f
 import { EventStorage } from '../../../common/api/EventStorage';
 import EventStoragePresenceHelper, { DateRange } from './EventStoragePresenceHelper';
 
+/**
+ * Class used to prefill days in instance of EventStorage interface
+ * with empty arrays.
+ *
+ * @remarks
+ * Take a note that preFillStorage method only works for date
+ * ranges that are defined in EventStoragePresenceHelper class.
+ *
+ * @internal
+ */
 export default class EventStorageFiller {
+    /**
+     * Fills closest date range with empty arrays.
+     *
+     * @param from
+     * @param till
+     * @param eventStorage instance of EventStorage interface.
+     */
     public static preFillStorage(from: Date, till: Date, eventStorage: EventStorage): EventStorage {
-        let dateRange: DateRange = EventStoragePresenceHelper.determineRange(from, till);
+        // Use PresenceHelper to determine DateRange that is the closest to the one provided by user.
+        const dateRange: DateRange = EventStoragePresenceHelper.determineRange(from, till);
 
         switch (dateRange) {
             case DateRange.DAY:
@@ -20,6 +38,12 @@ export default class EventStorageFiller {
         }
     }
 
+    /**
+     * Fills a single day with empty array.
+     *
+     * @param dayDate day that will be filled with empty array.
+     * @param eventStorage instance of EventStorage interface.
+     */
     private static fillDay(dayDate: Date, eventStorage: EventStorage): EventStorage {
         let eventStorageCopy = eventStorage;
 
@@ -40,6 +64,12 @@ export default class EventStorageFiller {
         return eventStorageCopy;
     }
 
+    /**
+     * Fills days in week with empty array.
+     *
+     * @param weekDay any day of target week.
+     * @param eventStorage instance of EventStorage interface.
+     */
     private static fillWeek(weekDay: Date, eventStorage: EventStorage): EventStorage {
         const weekDays: Date[] = eachDayOfInterval({ start: startOfWeek(weekDay), end: endOfWeek(weekDay) });
 
@@ -50,6 +80,12 @@ export default class EventStorageFiller {
         return eventStorage;
     }
 
+    /**
+     * Fills days in month with empty array.
+     *
+     * @param monthDay any day of target month
+     * @param eventStorage instance of EventStorage interface.
+     */
     private static fillMonth(monthDay: Date, eventStorage: EventStorage): EventStorage {
         const monthDays: Date[] = eachDayOfInterval({ start: startOfMonth(monthDay), end: endOfMonth(monthDay) });
         let storageCopy = eventStorage;
