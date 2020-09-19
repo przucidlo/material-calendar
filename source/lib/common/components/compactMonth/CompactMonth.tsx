77165174c18dc8a180cdafbc398ed12523004f9f
Patrick <patrykrzucidlo1@gmail.com>
2020-09-19 17:25:38 +0200

    Create CompactMonth component

---
+++ source/lib/common/components/compactMonth/CompactMonth.tsx
@@ -0,0 +1,35 @@
+import { Box, makeStyles, Typography } from '@material-ui/core';
+import React, { ReactElement, ReactNode } from 'react';
+import useLocale from '../../hooks/locale/useLocale';
+import CompactMonthGrid from './components/grid/CompactMonthGrid';
+import CompactMonthGridHeader from './components/grid/CompactMonthGridHeader';
+
+export interface CompactMonthProps {
+    month: Date;
+}
+
+const useStyles = makeStyles((theme) => ({
+    content: {
+        minWidth: 224,
+        minHeight: 250,
+        maxHeight: 250,
+        padding: 8,
+    },
+}));
+
+export default function CompactMonth(props: CompactMonthProps): ReactElement {
+    const classes = useStyles();
+    const locale = useLocale();
+
+    return (
+        <Box display="flex" justifyContent="center">
+            <div className={classes.content}>
+                <Typography variant="subtitle1" gutterBottom>
+                    {locale.months[props.month.getMonth()]}
+                </Typography>
+                <CompactMonthGridHeader />
+                <CompactMonthGrid month={props.month} />
+            </div>
+        </Box>
+    );
+}
