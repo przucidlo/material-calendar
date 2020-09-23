531c9eb0c0181a163e7e5a89383ebc603e634ab3
Patrick <patrykrzucidlo1@gmail.com>
2020-09-23 15:09:50 +0200

    Fix overflow in YearView

--- source/lib/views/year/YearView.tsx
+++ source/lib/views/year/YearView.tsx
@@ -1,12 +1,23 @@
-import { Grid } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import { endOfYear, startOfYear } from 'date-fns';
 import { eachMonthOfInterval } from 'date-fns/esm';
 import React, { Fragment, ReactElement, ReactFragment, useContext } from 'react';
 import CompactMonth from '../../common/components/compactMonth/CompactMonth';
 import { ViewContext } from '../../common/contexts/ViewContext';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: 'calc(100vh - 64px)',
+        overflowY: 'auto',
+    },
+    grid: {
+        marginTop: 4,
+    },
+}));
+
 export default function YearView(): ReactElement {
     const viewContext = useContext(ViewContext);
+    const classes = useStyles();
 
     function createGridElements(): ReactFragment[] {
         return eachMonthOfInterval({
@@ -22,14 +33,10 @@ export default function YearView(): ReactElement {
     }
 
     return (
-        <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            style={{ minHeight: 'calc(100% - 64px)', marginTop: 16 }}
-        >
-            {createGridElements()}
-        </Grid>
+        <div className={classes.root}>
+            <Grid container direction="row" justify="center" alignItems="center" className={classes.grid}>
+                {createGridElements()}
+            </Grid>
+        </div>
     );
 }
