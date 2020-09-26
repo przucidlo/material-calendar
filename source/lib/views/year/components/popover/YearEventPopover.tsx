fc3456ad394c37636148cf8c43999ef9066e407a
Patrick <patrykrzucidlo1@gmail.com>
2020-09-26 16:40:38 +0200

    Create YearEventPopover

---
+++ source/lib/views/year/components/popover/YearEventPopover.tsx
@@ -0,0 +1,45 @@
+import { makeStyles, Popover } from '@material-ui/core';
+import React, { ReactElement, useContext } from 'react';
+import DateEvents from '../../../../common/components/dateEvents/DateEvents';
+import { EventStorageContext } from '../../../../common/contexts/EventStorageContext';
+import { ViewContext } from '../../../../common/contexts/ViewContext';
+import bindPopover from '../../../../common/hooks/popover/bindPopover';
+import { PopoverState } from '../../../../common/hooks/popover/usePopover';
+
+interface YearEventPopoverProps {
+    popoverState: PopoverState;
+}
+
+const useStyles = makeStyles((theme) => ({
+    popover: {
+        width: 224,
+    },
+}));
+
+export default function YearEventPopover(props: YearEventPopoverProps): ReactElement {
+    const eventStorageContext = useContext(EventStorageContext);
+    const viewContext = useContext(ViewContext);
+    const classes = useStyles();
+
+    return (
+        <Popover
+            {...bindPopover(props.popoverState)}
+            anchorOrigin={{
+                vertical: 'center',
+                horizontal: 'center',
+            }}
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+            }}
+        >
+            <div className={classes.popover}>
+                <DateEvents
+                    date={viewContext.highlightDate}
+                    onClose={props.popoverState.closePopover}
+                    eventStorage={eventStorageContext.eventStorage}
+                />
+            </div>
+        </Popover>
+    );
+}
