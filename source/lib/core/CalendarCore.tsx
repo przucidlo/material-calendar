f78f798618f0a6f2ec01e5008eed712669a5df2d
Patrick <patrykrzucidlo1@gmail.com>
2020-09-26 16:22:12 +0200

    Use memo is CalendarCore

--- source/lib/core/CalendarCore.tsx
+++ source/lib/core/CalendarCore.tsx
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core';
 import 'fontsource-roboto';
-import React, { ReactElement } from 'react';
+import React, { memo, ReactElement } from 'react';
 import NavigationBar from './components/navigationBar/NavigationBar';
 import ViewController from './components/viewController/ViewController';
 
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function CalendarCore(): ReactElement {
+function CalendarCore(): ReactElement {
     const classes = useStyles();
 
     return (
@@ -21,3 +21,5 @@ export default function CalendarCore(): ReactElement {
         </div>
     );
 }
+
+export default memo(CalendarCore);
