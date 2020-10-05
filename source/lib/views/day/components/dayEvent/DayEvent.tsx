c7dd5887e7f4d25a257a66406a1062a4bca539fd
Patrick <patrykrzucidlo1@gmail.com>
2020-10-05 16:07:13 +0200

    Refractor DayEvent to support refractor of DayGrid

--- source/lib/views/day/components/dayEvent/DayEvent.tsx
+++ source/lib/views/day/components/dayEvent/DayEvent.tsx
@@ -1,5 +1,5 @@
-import { Box, Paper, Popover, useTheme } from '@material-ui/core';
-import { format } from 'date-fns';
+import { makeStyles, Paper, Popover } from '@material-ui/core';
+import { differenceInMinutes, format } from 'date-fns';
 import React, { ReactElement } from 'react';
 import CalendarEvent from '../../../../common/api/CalendarEvent';
 import toggleEventPopover from '../../../../common/hooks/eventPopover/toggleEventPopover';
@@ -8,15 +8,28 @@ import bindPopover from '../../../../common/hooks/popover/bindPopover';
 import usePopover from '../../../../common/hooks/popover/usePopover';
 
 export interface DayEvent {
-    height: number;
-    positionTop: number;
     calendarEvent: CalendarEvent;
+    cellHeight: number;
+
+    orderInChain: number;
+    chainCount: { count: number };
 }
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        color: 'white',
+        position: 'absolute',
+    },
+    paper: {
+        backgroundColor: theme.palette.primary.main,
+        color: 'white',
+    },
+}));
+
 export default function DayEvent(props: DayEvent) {
-    const theme = useTheme();
     const eventPopover = useEventPopover();
     const popoverState = usePopover();
+    const classes = useStyles();
 
     function getPopoverContent(): ReactElement | null {
         if (eventPopover) {
@@ -28,44 +41,104 @@ export default function DayEvent(props: DayEvent) {
         return null;
     }
 
+    /**
+     * Divides length of the event into the sections
+     * and multiplies it by size of the cell.
+     */
+    function getHeight(): number {
+        const durationInMinutes = differenceInMinutes(props.calendarEvent.finishedAt, props.calendarEvent.startedAt);
+        const numberOfSections = Math.round(durationInMinutes / 15);
+
+        return numberOfSections * (props.cellHeight / 4);
+    }
+
+    /**
+     * Calculates remaining width of the element by
+     * subtracking positionInChain from 100%.
+     */
+    function getWidth(): string {
+        return String(100 - getPositionInChain() + '%');
+    }
+
+    /**
+     * Calculates X position of the element based on
+     * size of the chainCount and orderInChain of the element.
+     */
+    function getPositionLeft(): string {
+        return String(getPositionInChain() + '%');
+    }
+
+    /**
+     * Calculates which part of the chain belongs
+     * to this element.
+     */
+    function getPositionInChain(): number {
+        return (100 / props.chainCount.count) * (props.orderInChain - 1);
+    }
+
+    /**
+     * Calculates Y position of the element based on
+     * when the event startedAt property.
+     */
+    function getPositionTop(): number {
+        const startedAt = props.calendarEvent.startedAt;
+
+        // (hours + (minutes/hour)) * cellHeight.
+        return (startedAt.getHours() + roundMinutes(startedAt) / 60) * props.cellHeight;
+    }
+
+    /**
+     * Rounds minutes to provided values to make
+     * display of the grid "snappy".
+     */
+    function roundMinutes(startedAt: Date): number {
+        const startedAtMinutes = startedAt.getMinutes();
+        const points: number[] = [45, 30, 15, 0];
+
+        for (let point of points) {
+            if (startedAtMinutes >= point) {
+                return point;
+            }
+        }
+
+        return 0;
+    }
+
     return (
         <div
             style={{
-                width: '100%',
-                height: props.height,
-                color: 'white',
-                top: props.positionTop,
-                position: 'absolute',
+                width: getWidth(),
+                height: getHeight(),
+                top: getPositionTop(),
+                left: getPositionLeft(),
             }}
+            className={classes.root}
         >
-            <Box pr={0}>
-                <Paper
-                    variant="outlined"
-                    style={{
-                        minHeight: props.height,
-                        backgroundColor: theme.palette.primary.main,
-                        color: 'white',
-                    }}
-                    elevation={0}
-                    {...toggleEventPopover(popoverState, eventPopover)}
-                >
-                    {props.calendarEvent.title}, {format(props.calendarEvent.startedAt, 'HH:mm')} -{' '}
-                    {format(props.calendarEvent.finishedAt, 'HH:mm')}
-                </Paper>
-                <Popover
-                    {...bindPopover(popoverState)}
-                    anchorOrigin={{
-                        vertical: 'center',
-                        horizontal: 'center',
-                    }}
-                    transformOrigin={{
-                        vertical: 'center',
-                        horizontal: 'center',
-                    }}
-                >
-                    {getPopoverContent()}
-                </Popover>
-            </Box>
+            <Paper
+                variant="outlined"
+                style={{
+                    minHeight: getHeight(),
+                }}
+                elevation={0}
+                className={classes.paper}
+                {...toggleEventPopover(popoverState, eventPopover)}
+            >
+                {props.calendarEvent.title}, {format(props.calendarEvent.startedAt, 'HH:mm')} -{' '}
+                {format(props.calendarEvent.finishedAt, 'HH:mm')}
+            </Paper>
+            <Popover
+                {...bindPopover(popoverState)}
+                anchorOrigin={{
+                    vertical: 'center',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'center',
+                    horizontal: 'center',
+                }}
+            >
+                {getPopoverContent()}
+            </Popover>
         </div>
     );
 }
