9cbd233ed3a321659a1f113861a9b277d08b100b
Patrick <patrykrzucidlo1@gmail.com>
2020-10-05 16:07:56 +0200

    Refractor DayEventGrid

--- source/lib/views/day/components/dayEvent/DayEventGrid.tsx
+++ source/lib/views/day/components/dayEvent/DayEventGrid.tsx
@@ -1,46 +1,85 @@
-import { differenceInMinutes } from 'date-fns/esm';
+import { makeStyles } from '@material-ui/core';
+import { areIntervalsOverlapping } from 'date-fns';
 import React, { ReactElement } from 'react';
 import CalendarEvent from '../../../../common/api/CalendarEvent';
 import DayEvent from './DayEvent';
 
 export interface DayEventGridProps {
-    dayEvents: CalendarEvent[];
+    /**
+     * Sorted array of calendarEvents from oldest to newest event.
+     */
+    calendarEvents: CalendarEvent[];
 }
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        position: 'absolute',
+        width: '100%',
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+    },
+}));
+
+/*
+ * Every cell in the grid must have the same height,
+ * otherwise grid won't be displayed correctly.
+ */
+const cellHeight = 48;
+
 export default function DayEventGrid(props: DayEventGridProps): ReactElement {
-    const elementSize = 48;
-    const rowHourRatio = elementSize / 60;
+    const classes = useStyles();
 
-    function renderDayEvents(): ReactElement[] {
-        return props.dayEvents.map((calendarEvent) => (
-            <div key={calendarEvent.startedAt.toTimeString()}>
-                <DayEvent
-                    calendarEvent={calendarEvent}
-                    positionTop={calculateElementPosition(calendarEvent.startedAt)}
-                    height={calculateElementHeight(calendarEvent.startedAt, calendarEvent.finishedAt)}
-                />
-            </div>
-        ));
+    /*
+     *  Render methods.
+     */
+
+    function renderGrid(): ReactElement[] {
+        let elements: ReactElement[] = [];
+
+        let chainCount = {
+            count: 1,
+        };
+
+        for (let i = 0; i < props.calendarEvents.length; i++) {
+            const previousElement: CalendarEvent | undefined = props.calendarEvents[i - 1];
+            const currentElement = props.calendarEvents[i];
+
+            if (areElementsOverlapping(previousElement, currentElement)) {
+                chainCount.count += 1;
+            } else {
+                chainCount = { count: 1 };
+            }
+
+            elements.push(
+                <div key={currentElement.id}>
+                    <DayEvent
+                        calendarEvent={currentElement}
+                        cellHeight={cellHeight}
+                        orderInChain={Object.assign({}, chainCount).count}
+                        chainCount={chainCount}
+                    />
+                </div>,
+            );
+        }
+
+        return elements;
     }
 
-    function calculateElementHeight(startedAt: Date, finishedAt: Date): number {
-        const durationInMinutes = differenceInMinutes(finishedAt, startedAt);
-        const result = rowHourRatio * durationInMinutes;
-
-        if (result <= elementSize / 2) return elementSize / 2;
-
-        return result;
-    }
-
-    function calculateElementPosition(startedAt: Date): number {
-        return (startedAt.getHours() + startedAt.getMinutes() / 60) * elementSize;
+    function areElementsOverlapping(previousElement: CalendarEvent, currentElement: CalendarEvent): boolean {
+        if (previousElement) {
+            return areIntervalsOverlapping(
+                { start: previousElement.startedAt, end: previousElement.finishedAt },
+                { start: currentElement.startedAt, end: currentElement.finishedAt },
+            );
+        }
+        return false;
     }
 
     return (
-        <div>
-            <div style={{ position: 'absolute', width: '100%', left: 0, top: 0, right: 0, bottom: 0 }}>
-                {renderDayEvents()}
-            </div>
+        <div className={classes.root}>
+            <div style={{}}>{renderGrid()}</div>
         </div>
     );
 }
