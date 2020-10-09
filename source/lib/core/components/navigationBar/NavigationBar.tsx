e363a4fdb4741f63ebcb627925f7e39588a58eb5
Patrick <patrykrzucidlo1@gmail.com>
2020-10-09 14:51:22 +0200

    Export NAVIGATION_BAR_HEIGHT field from NavigationBar component

--- source/lib/core/components/navigationBar/NavigationBar.tsx
+++ source/lib/core/components/navigationBar/NavigationBar.tsx
@@ -3,16 +3,18 @@ import React, { ReactElement } from 'react';
 import NavigationBarControls from './NavigationBarControls';
 import NavigationBarViewSelect from './NavigationBarViewSelect';
 
+export const NAVIGATION_BAR_HEIGHT = 63;
+
 const useStyles = makeStyles((theme) => ({
     navigationBar: {
-        height: 63,
+        height: NAVIGATION_BAR_HEIGHT,
         width: '100%',
         background: theme.palette.common.white,
         borderBottom: '1px solid',
         borderBottomColor: theme.palette.grey[300],
     },
     gridStyle: {
-        height: 63,
+        height: NAVIGATION_BAR_HEIGHT,
         top: 0,
         position: 'sticky',
     },
