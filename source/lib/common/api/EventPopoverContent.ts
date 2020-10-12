aeb7d38dd284071ee5f896afd8222c709b939bb2
Patrick <patrykrzucidlo1@gmail.com>
2020-10-12 17:22:49 +0200

    Fix export in EventPopoverContent

--- source/lib/common/api/EventPopoverContent.ts
+++ source/lib/common/api/EventPopoverContent.ts
@@ -1,3 +1,5 @@
 import { ComponentClass, FunctionComponent } from 'react';
 
 export type EventPopoverContent = FunctionComponent<any> | ComponentClass<any, any> | undefined;
+
+export default EventPopoverContent;
