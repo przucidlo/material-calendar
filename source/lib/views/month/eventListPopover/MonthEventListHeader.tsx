2f6928f3edda348959d671bf3eedeffaf29f0a41
Patrick <patrykrzucidlo1@gmail.com>
2020-10-13 20:19:34 +0200

    Add openChildView to MonthEventListHeader

--- source/lib/views/month/eventListPopover/MonthEventListHeader.tsx
+++ source/lib/views/month/eventListPopover/MonthEventListHeader.tsx
@@ -2,6 +2,8 @@ import { makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 import DateAvatar from '../../../common/components/dateAvatar/DateAvatar';
 import useLocale from '../../../common/hooks/locale/useLocale';
+import useViewChange from '../../../common/hooks/viewController/useViewChange';
+import DayView from '../../day/DayView';
 
 export interface MonthEventListHeaderProps {
     date: Date;
@@ -16,15 +18,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function MonthEventListHeader(props: MonthEventListHeaderProps) {
+    const viewChange = useViewChange();
     const classes = useStyles();
     const locale = useLocale();
     
+    function openChildView(event: React.MouseEvent<any, MouseEvent>, date: Date): void {
+        viewChange.changeView(DayView, date);
+    }
+
     return (
         <div className={classes.root}>
             <Typography align="center" variant="subtitle2" gutterBottom>
                 {locale.daysShort[props.date.getDay()]}
             </Typography>
-            <DateAvatar date={props.date} highlightOnHover/>
+            <DateAvatar date={props.date} highlightOnHover onClick={openChildView}/>
         </div>
     );
 }
