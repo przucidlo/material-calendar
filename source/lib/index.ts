90838e9046a0faa57244502a7fecb0ff510e976a
Patrick <patrykrzucidlo1@gmail.com>
2021-07-20 19:32:35 +0200

    Export ScheduleViewEvent from index.ts

--- source/lib/index.ts
+++ source/lib/index.ts
@@ -1,31 +1,33 @@
-
 // COMMON/API
 
-export {default as CalendarEvent} from './common/api/CalendarEvent';
+export { default as CalendarEvent } from './common/api/CalendarEvent';
 export * from './common/api/CalendarEvent';
 
-export {default as CalendarEventStorage} from './common/api/CalendarEventStorage';
+export { default as CalendarEventStorage } from './common/api/CalendarEventStorage';
 export * from './common/api/CalendarEventStorage';
 
-export {default as CalendarView} from './common/api/CalendarView';
+export { default as CalendarView } from './common/api/CalendarView';
 export * from './common/api/CalendarView';
 
-export {default as DateChangeAction} from './common/api/DateChangeAction';
+export { default as DateChangeAction } from './common/api/DateChangeAction';
 export * from './common/api/DateChangeAction';
 
-export {default as EventPopoverContent} from './common/api/EventPopoverContent';
+export { default as EventPopoverContent } from './common/api/EventPopoverContent';
 export * from './common/api/EventPopoverContent';
 
-export {default as EventStorage} from './common/api/EventStorage';
+export { default as EventStorage } from './common/api/EventStorage';
 export * from './common/api/EventStorage';
 
-export {default as LocalizationElements} from './common/api/LocalizationElements';
+export { default as LocalizationElements } from './common/api/LocalizationElements';
 export * from './common/api/LocalizationElements';
 
 // COMMON/COMPONENTS
 
-export {default as ExamplePopover} from './common/components/examplePopover/ExamplePopover';
+export { default as ExamplePopover } from './common/components/examplePopover/ExamplePopover';
 export * from './common/components/examplePopover/ExamplePopover';
+
+export { default as ScheduleViewEvent } from './views/schedule/event/ScheduleViewEvent';
+export * from './views/schedule/event/ScheduleViewEvent';
 
 // COMMON/CONTEXTS
 
@@ -37,53 +39,53 @@ export * from './common/contexts/ViewContext';
 
 // COMMON/HOOKS
 
-export {default as toggleEventPopover} from './common/hooks/eventPopover/toggleEventPopover';
+export { default as toggleEventPopover } from './common/hooks/eventPopover/toggleEventPopover';
 export * from './common/hooks/eventPopover/toggleEventPopover';
 
-export {default as useEventPopover} from './common/hooks/eventPopover/useEventPopover';
+export { default as useEventPopover } from './common/hooks/eventPopover/useEventPopover';
 export * from './common/hooks/eventPopover/useEventPopover';
 
-export {default as useLocale} from './common/hooks/locale/useLocale';
+export { default as useLocale } from './common/hooks/locale/useLocale';
 export * from './common/hooks/locale/useLocale';
 
-export {default as bindPopover} from './common/hooks/popover/bindPopover';
+export { default as bindPopover } from './common/hooks/popover/bindPopover';
 export * from './common/hooks/popover/bindPopover';
 
-export {default as togglePopover} from './common/hooks/popover/togglePopover';
+export { default as togglePopover } from './common/hooks/popover/togglePopover';
 export * from './common/hooks/popover/togglePopover';
 
-export {default as usePopover} from './common/hooks/popover/usePopover';
+export { default as usePopover } from './common/hooks/popover/usePopover';
 export * from './common/hooks/popover/usePopover';
 
-export {default as useViewChange} from './common/hooks/viewController/useViewChange';
-export * from  './common/hooks/viewController/useViewChange';
+export { default as useViewChange } from './common/hooks/viewController/useViewChange';
+export * from './common/hooks/viewController/useViewChange';
 
 // CORE
 
-export {default as MaterialCalendar} from './core/MaterialCalendar';
+export { default as MaterialCalendar } from './core/MaterialCalendar';
 export * from './core/MaterialCalendar';
 
 // LOCALE
 
-export {default as LocaleList} from './locale/LocaleList';
+export { default as LocaleList } from './locale/LocaleList';
 export * from './locale/LocaleList';
 
 // VIEWS
 
-export {default as DayView} from './views/day/DayView';
+export { default as DayView } from './views/day/DayView';
 export * from './views/day/DayView';
 
-export {default as WeekView} from './views/week/WeekView';
+export { default as WeekView } from './views/week/WeekView';
 export * from './views/week/WeekView';
 
-export {default as MonthView} from './views/month/MonthView';
+export { default as MonthView } from './views/month/MonthView';
 export * from './views/month/MonthView';
 
-export {default as YearView} from './views/year/YearView';
+export { default as YearView } from './views/year/YearView';
 export * from './views/year/YearView';
 
-export {default as ScheduleView} from './views/schedule/ScheduleView';
+export { default as ScheduleView } from './views/schedule/ScheduleView';
 export * from './views/schedule/ScheduleView';
 
-export {default as defaultViews} from './views/DefaultViews';
+export { default as defaultViews } from './views/DefaultViews';
 export * from './views/DefaultViews';
