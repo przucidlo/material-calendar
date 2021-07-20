b26ccccc28a456b78d6737cf5902b54baf20248e
Patrick <patrykrzucidlo1@gmail.com>
2021-07-20 21:22:56 +0200

    Move core.css into makeStyles of CalendarCore

--- source/lib/core/CalendarCore.tsx
+++ source/lib/core/CalendarCore.tsx
@@ -3,12 +3,31 @@ import 'fontsource-roboto';
 import React, { memo, ReactElement } from 'react';
 import NavigationBar from './components/navigationBar/NavigationBar';
 import ViewController from './components/viewController/ViewController';
-import './core.css';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         backgroundColor: theme.palette.common.white,
         fontFamily: 'Roboto',
+    },
+    '@global': {
+        '*::-webkit-scrollbar': {
+            WebkitAppearance: 'none',
+        },
+        '*::-webkit-scrollbar:vertical': {
+            width: '16px',
+        },
+        '*::-webkit-scrollbar:horizontal': {
+            height: '16px',
+        },
+        '*::-webkit-scrollbar-thumb': {
+            borderRadius: '8px',
+            backgroundColor: 'rgb(190, 193, 198)',
+            border: '4px solid white',
+        },
+        '*::webkit-scrollbar-track': {
+            backgroundColor: '#fff',
+            borderRadius: '8px',
+        },
     },
 }));
 
